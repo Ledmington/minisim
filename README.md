129fd112a457c8e84dc78080ec832275f8d74b09
Ledmington <ledmington.dev@gmail.com>
2022-09-11 23:16:50 +0200

    Various modifications

---
+++ README.md
@@ -0,0 +1,8 @@
+# MiniSim
+Minimal N-bodies simulation written with Java + JavaFX + Gradle
+
+## Local development
+This command represents the entire development worflow:
+```bash
+./gradlew spotlessApply build jacocoTestReport createExe run
+```
--- app/src/main/java/minisim/App.java
+++ app/src/main/java/minisim/App.java
@@ -1,25 +1,86 @@
 package minisim;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class App extends Application {
 
-@Override
-public void start(Stage stage) {
-	String javaVersion = System.getProperty("java.version");
-	String javafxVersion = System.getProperty("javafx.version");
-	Label l =
-		new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-	Scene scene = new Scene(new StackPane(l), 640, 480);
-	stage.setScene(scene);
-	stage.show();
-}
+	public static Logger logger = null;
 
-public static void main(String[] args) {
-	launch();
-}
+	public static final String javaVersion = System.getProperty("java.version");
+	public static final String javafxVersion = System.getProperty("javafx.version");
+
+	static {
+		InputStream stream = App.class.getClassLoader().getResourceAsStream("logging.properties");
+		try {
+			LogManager.getLogManager().readConfiguration(stream);
+			logger = Logger.getLogger("minisim");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void start(Stage stage) {
+		BorderPane bPane = new BorderPane();
+
+		final Button aboutButton = new Button("About");
+		aboutButton.setOnAction(e -> {
+			final Dialog<String> dialog = new Dialog<>();
+			dialog.setHeaderText("About MiniSim");
+			dialog.setContentText(
+					String.join("\n", "MiniSim is running on:", "Java " + javaVersion, "JavaFX " + javafxVersion));
+			dialog.setResizable(false);
+			Window window = dialog.getDialogPane().getScene().getWindow();
+			window.setOnCloseRequest(event -> window.hide());
+			dialog.showAndWait();
+		});
+
+		bPane.setTop(aboutButton);
+		bPane.setBottom(new Label("MiniSim v0.1.0"));
+
+		final Canvas canvas = new Canvas(300, 300);
+		final GraphicsContext gc = canvas.getGraphicsContext2D();
+		Stop[] stops;
+		LinearGradient gradient;
+
+		stops = new Stop[]{new Stop(0, Color.RED), new Stop(1, Color.BLUE)};
+		gradient = new LinearGradient(0.5, 0, 0.5, 1, true, CycleMethod.NO_CYCLE, stops);
+		gc.setFill(gradient);
+		gc.fillRect(0, 0, 300, 300);
+		gc.fill();
+		gc.stroke();
+
+		bPane.setCenter(canvas);
+
+		Scene scene = new Scene(bPane);
+
+		stage.setTitle("MiniSim");
+		stage.setScene(scene);
+		stage.show();
+	}
+	public static void main(String args[]) {
+		logger.info("MiniSim is running on:");
+		logger.info(" - Java " + javaVersion);
+		logger.info(" - JavaFX " + javafxVersion);
+		launch(args);
+	}
 }
