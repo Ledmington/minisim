46c6f2f044a7d722c73e63dec301389dcbdc7d79
Ledmington <ledmington.dev@gmail.com>
2022-09-13 14:40:52 +0200

    Minor modifications

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,8 +1,5 @@
 module minisim.java.app.main {
-	requires javafx.controls;
 	requires java.logging;
-
-	exports minisim;
-	exports minisim.simulation.border;
-	exports minisim.simulation;
+	requires javafx.graphics;
+	requires javafx.controls;
 }
