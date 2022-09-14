9084ca3a94b4fb55ec45a8dd9be4c4c06d1ce7e6
Ledmington <ledmington.dev@gmail.com>
2022-09-14 23:41:54 +0200

    Implemented complete test suite about Friction

--- app/src/main/java/minisim/simulation/force/Friction.java
+++ app/src/main/java/minisim/simulation/force/Friction.java
@@ -7,11 +7,14 @@ public class Friction implements UnaryForce {
 	private final double constant;
 
 	public Friction(final double constant) {
-		this.constant = constant;
+		if (constant < 0 || constant > 1) {
+			throw new IllegalArgumentException("Friction constant must be in [0;1]");
+		}
+		this.constant = 1 - constant;
 	}
 
 	@Override
 	public void accept(final Body body) {
-		body.force.mul(constant);
+		body.force.sub(body.force.copy().mul(body.mass * constant));
 	}
 }
--- app/src/test/java/minisim/force/TestFriction.java
+++ app/src/test/java/minisim/force/TestFriction.java
@@ -1,4 +1,89 @@
 package minisim.force;
 
+import minisim.simulation.Body;
+import minisim.simulation.V2;
+import minisim.simulation.force.Friction;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestFriction {
+
+	@Test
+	public void invalidFrictionConstant() {
+		for (Double v : List.of(-0.1, -3.5, -12.0, 1.1, 1.01, 1.001, 6.2, 32.0)) {
+			assertThrows(IllegalArgumentException.class, () -> new Friction(v),
+					"Friction constant should be in range [0;1]");
+		}
+	}
+
+	@Test
+	public void frictionConstantZeroIsAllowed() {
+		final Body b = new Body(new V2(2, 3), V2.origin(), 1, 1);
+		final Friction friction = new Friction(0);
+
+		b.force = new V2(3, -5);
+
+		friction.accept(b);
+
+		assertEquals(0, b.force.mod());
+	}
+
+	@Test
+	public void frictionConstantOneIsAllowed() {
+		final Body b = new Body(new V2(2, 3), V2.origin(), 1, 1);
+		final Friction friction = new Friction(1);
+
+		final V2 oldForce = new V2(3, -5);
+		b.force = oldForce.copy();
+
+		friction.accept(b);
+
+		assertEquals(oldForce, b.force);
+	}
+
+	@Test
+	public void stationaryObjectShouldRemainStationary() {
+		final Body b = new Body(new V2(2, 3), V2.origin(), 1, 1);
+		final Friction friction = new Friction(0.9);
+
+		final V2 oldPosition = b.position.copy();
+
+		friction.accept(b);
+		b.applyForce();
+
+		assertEquals(oldPosition, b.position);
+	}
+
+	@Test
+	public void alreadyPresentForceShouldDecreaseInModulo() {
+		final Body b = new Body(new V2(2, 3), V2.origin(), 1, 1);
+		final Friction friction = new Friction(0.9);
+
+		final V2 oldForce = new V2(3, -5);
+
+		b.force = oldForce.copy();
+
+		friction.accept(b);
+
+		assertTrue(b.force.mod() < oldForce.mod());
+	}
+
+	@Test
+	public void heavierObjectsForceShouldDecreaseMore() {
+		final Body light = new Body(new V2(2, 3), V2.origin(), 2, 1);
+		final Body heavy = new Body(new V2(7, 4), V2.origin(), 4, 1);
+		final Friction friction = new Friction(0.9);
+
+		final V2 oldForce = new V2(3, -5);
+		light.force = oldForce.copy();
+		heavy.force = oldForce.copy();
+
+		friction.accept(light);
+		friction.accept(heavy);
+
+		assertTrue(light.force.mod() > heavy.force.mod());
+	}
 }
