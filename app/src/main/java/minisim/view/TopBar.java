da067686bdb0307f17980c93f07b842fbb930a98
Ledmington <ledmington.dev@gmail.com>
2022-09-18 08:43:21 +0200

    Improved topbar

--- app/src/main/java/minisim/view/TopBar.java
+++ app/src/main/java/minisim/view/TopBar.java
@@ -7,9 +7,19 @@ import javafx.scene.control.MenuItem;
 public class TopBar extends MenuBar {
 	public TopBar() {
 		final Menu fileMenu = new Menu("File");
+		fileMenu.getItems().add(new MenuItem("New Simulation"));
+		fileMenu.getItems().add(new MenuItem("Save Simulation"));
+		fileMenu.getItems().add(new MenuItem("Load Simulation"));
+		final MenuItem exitButton = new MenuItem("Exit");
+		exitButton.setOnAction(ViewUtils.showExitDialog());
+		fileMenu.getItems().add(exitButton);
+
 		final Menu optionsMenu = new Menu("Options");
 
 		final Menu helpMenu = new Menu("Help");
+		final MenuItem authorsButton = new MenuItem("Authors & Contributions");
+		authorsButton.setOnAction(ViewUtils.showAuthorsDialog());
+		helpMenu.getItems().add(authorsButton);
 		final MenuItem aboutButton = new MenuItem("About");
 		aboutButton.setOnAction(ViewUtils.showAboutDialog());
 		helpMenu.getItems().add(aboutButton);
--- app/src/main/java/minisim/view/ViewUtils.java
+++ app/src/main/java/minisim/view/ViewUtils.java
@@ -4,8 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import java.util.Optional;
 
 import static minisim.AppConstants.*;
 
@@ -23,15 +27,34 @@ public class ViewUtils {
 			final Scene scene = new Scene(pane);
 			stage.setScene(scene);
 			stage.showAndWait();
-			/*
-			 * final Dialog<String> dialog = new Dialog<>();
-			 * dialog.setHeaderText("About MiniSim");
-			 * dialog.setContentText(String.join("\n", "MiniSim is running on:", OSName +
-			 * " " + OSVersion, "Java " + javaVersion, "JVM " + jvmVersion, "JavaFX " +
-			 * javafxVersion)); dialog.setResizable(false); Window window =
-			 * dialog.getDialogPane().getScene().getWindow(); window.setOnCloseRequest(e ->
-			 * window.hide()); dialog.showAndWait();
-			 */
+		};
+	}
+
+	public static EventHandler<ActionEvent> showAuthorsDialog() {
+		return event -> {
+			final Stage stage = new Stage();
+			stage.setTitle("MiniSim's authors and contributions");
+			final BorderPane pane = new BorderPane();
+			pane.setTop(new Label("Authors and Contributions"));
+			pane.setCenter(new Label("Ledmington"));
+			stage.setResizable(false);
+			final Scene scene = new Scene(pane);
+			stage.setScene(scene);
+			stage.showAndWait();
+		};
+	}
+
+	public static EventHandler<ActionEvent> showExitDialog() {
+		return event -> {
+			final ButtonType yes = new ButtonType("Yes", ButtonBar.ButtonData.OK_DONE);
+			final ButtonType no = new ButtonType("No", ButtonBar.ButtonData.CANCEL_CLOSE);
+			final Alert alert = new Alert(Alert.AlertType.WARNING,
+					"If you exit now, all your unsaved data will be lost.\nDo you wish to exit?", yes, no);
+			alert.setTitle("Exit");
+			final Optional<ButtonType> result = alert.showAndWait();
+			if (result.orElse(no) == yes) {
+				System.exit(0);
+			}
 		};
 	}
 }
