03dac99b61eb36c19f9c879a62a2c36a30793cdd
Ledmington <ledmington.dev@gmail.com>
2023-09-07 23:06:07 +0200

    Vectorized Friction

--- model/src/main/java/com/ledmington/minisim/simulation/force/Friction.java
+++ model/src/main/java/com/ledmington/minisim/simulation/force/Friction.java
@@ -19,15 +19,21 @@ package com.ledmington.minisim.simulation.force;
 
 import com.ledmington.minisim.simulation.SimulationState;
 
+import jdk.incubator.vector.DoubleVector;
+import jdk.incubator.vector.VectorMask;
+import jdk.incubator.vector.VectorSpecies;
+
 public final class Friction implements Force {
 
-    private final double constant;
+    private static final VectorSpecies<Double> species = DoubleVector.SPECIES_PREFERRED;
+    private static final int lanes = species.length();
+    private final DoubleVector constant;
 
     public Friction(final double constant) {
         if (constant < 0 || constant > 1) {
             throw new IllegalArgumentException("Friction constant must be in [0;1]");
         }
-        this.constant = 1 - constant;
+        this.constant = DoubleVector.broadcast(species, 1 - constant);
     }
 
     @Override
@@ -35,9 +41,25 @@ public final class Friction implements Force {
         final double[] forcex = state.forcex();
         final double[] forcey = state.forcey();
         final double[] masses = state.masses();
-        for (int i = 0; i < forcex.length; i++) {
-            forcex[i] -= forcex[i] * masses[i] * constant;
-            forcey[i] -= forcey[i] * masses[i] * constant;
+        int i = 0;
+        for (; i + (lanes - 1) < forcex.length; i += lanes) {
+            DoubleVector masses_i = DoubleVector.fromArray(species, masses, i);
+            DoubleVector forcex_i = DoubleVector.fromArray(species, forcex, i);
+            DoubleVector forcey_i = DoubleVector.fromArray(species, forcey, i);
+            forcex_i = forcex_i.sub(forcex_i.mul(masses_i).mul(constant));
+            forcey_i = forcey_i.sub(forcey_i.mul(masses_i).mul(constant));
+            forcex_i.intoArray(forcex, i);
+            forcey_i.intoArray(forcey, i);
+        }
+        if (i < forcex.length) {
+            VectorMask<Double> mask = species.indexInRange(i, forcex.length);
+            DoubleVector masses_i = DoubleVector.fromArray(species, masses, i, mask);
+            DoubleVector forcex_i = DoubleVector.fromArray(species, forcex, i, mask);
+            DoubleVector forcey_i = DoubleVector.fromArray(species, forcey, i, mask);
+            forcex_i = forcex_i.sub(forcex_i.mul(masses_i).mul(constant));
+            forcey_i = forcey_i.sub(forcey_i.mul(masses_i).mul(constant));
+            forcex_i.intoArray(forcex, i, mask);
+            forcey_i.intoArray(forcey, i, mask);
         }
     }
 }
