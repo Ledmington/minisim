698fd6129715f9e6fb0762dd490e797d105addef
Ledmington <ledmington.dev@gmail.com>
2024-03-19 23:06:56 +0100

    Updated copyright year

--- view/src/main/java/com/ledmington/minisim/view/utils/EncoderUtils.java
+++ view/src/main/java/com/ledmington/minisim/view/utils/EncoderUtils.java
@@ -1,6 +1,6 @@
 /*
 * minisim - Minimalistic N-Body simulation
-* Copyright (C) 2022-2023 Filippo Barbari <filippo.barbari@gmail.com>
+* Copyright (C) 2022-2024 Filippo Barbari <filippo.barbari@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
